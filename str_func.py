0c0835d395893e024669dbab8ada8dab108c4603
AlexeyGonichev <dallarnn@gmail.com>
2023-07-18 12:10:39 +0300

    add docstrings and 2 func

--- str_func.py
+++ str_func.py
@@ -1,3 +1,7 @@
 def letters_down(value):
-    """выводит вструкц заглавными буквами"""
+    """меняет все буквы слова на заглавные"""
     return value.upper()
+
+def letters_is_down_1(str):
+    """меняет все первые буквы слова на заглавные"""
+    return str.capitalise()
